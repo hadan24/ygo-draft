b2dae28423bece946af6985c6f7463b664a0e984
hadan24 <ha.dan.t02@gmail.com>
2024-09-11 14:50:03 -0700

    Can terminal print retrieved card data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+use serde_derive::Deserialize;
+use serde_json::Value;
+
+#[derive(Deserialize, Debug)]
+struct Card {
+    data: Vec<HashMap<String, Value>>
+}
+
 fn main() {
+    let url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?";
+    let req = String::from(url) + "name=Tornado Dragon";
+
+    let resp = reqwest::blocking::get(req).unwrap();
+    let card: Card = resp.json().unwrap();
+
     println!("Hello, world!");
+    dbg!(card.data);
 }
