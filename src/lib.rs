3f26229948251ce053c5bdcedd00d52d1ccbb616
hadan24 <ha.dan.t02@gmail.com>
2024-09-23 18:00:15 -0700

    Set up yew + trunk. Temp disabled reqwest::blocking for trunk

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ mod card;
 
 #[derive(Debug, Deserialize)]
 pub struct YGOProResponse { pub data: Vec<card::Card> }
-
+/*
 pub fn get_cards(req: &str) -> YGOProResponse {
     let resp = reqwest::blocking::get(req).unwrap();
     
     resp.json().unwrap()
 }
+*/
