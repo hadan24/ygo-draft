368bd651ddf2219ec1b1f56baac900bcd8d8aa45
hadan24 <ha.dan.t02@gmail.com>
2024-09-25 22:22:20 -0700

    Can make simple async request, not properly error-handled

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use serde_derive::Deserialize;
+use std::error::Error;
 
 mod card;
 
 #[derive(Debug, Deserialize)]
 pub struct YGOProResponse { pub data: Vec<card::Card> }
 
-pub fn get_cards(req: &str) -> YGOProResponse {
-    let resp = reqwest::blocking::get(req).unwrap();
-    
-    resp.json().unwrap()
+pub async fn get_cards(req: &str) -> Result<YGOProResponse, Box<dyn Error>> {
+    Ok(
+        reqwest::get(req)
+        .await?
+        .json::<YGOProResponse>()
+        .await?
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-fn main() {
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?";
 
     let req = String::from(url) + "name="
@@ -9,8 +12,10 @@ fn main() {
         + "The Calculator|"     // test w/ `?` stat value
         + "Number F0: Utopic Future";   // test w/ no Level/Rank
 
-    let cards = ygo_draft::get_cards(&req);
+    let cards = ygo_draft::get_cards(&req).await?;
     for c in cards.data {
         dbg!(c);
     }
+
+    Ok(())
 }
