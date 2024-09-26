41e60c9fe2a2b411e8080b1cab78ce4fe9984b49
hadan24 <ha.dan.t02@gmail.com>
2024-09-25 22:00:07 -0700

    Revert "Set up yew + trunk. Temp disabled reqwest::blocking for trunk"
    This reverts commit 3f26229948251ce053c5bdcedd00d52d1ccbb616.

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,9 @@ mod card;
 
 #[derive(Debug, Deserialize)]
 pub struct YGOProResponse { pub data: Vec<card::Card> }
-/*
+
 pub fn get_cards(req: &str) -> YGOProResponse {
     let resp = reqwest::blocking::get(req).unwrap();
     
     resp.json().unwrap()
 }
-*/
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,4 @@
-use yew::prelude::*;
-
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
-    }
-}
-
 fn main() {
-    yew::Renderer::<App>::new().render();
-
     let url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?";
 
     let req = String::from(url) + "name="
@@ -31,10 +8,9 @@ fn main() {
         + "Solemn Judgment|"    // test w/ Trap
         + "The Calculator|"     // test w/ `?` stat value
         + "Number F0: Utopic Future";   // test w/ no Level/Rank
-/*
+
     let cards = ygo_draft::get_cards(&req);
     for c in cards.data {
         dbg!(c);
     }
-*/
 }
